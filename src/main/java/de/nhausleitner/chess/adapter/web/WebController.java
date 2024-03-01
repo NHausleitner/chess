b13b21207317ef4d56c9ad7f8c9ff52e6310c5bc
Nick Hausleitner <nick.hausleitner@gmail.com>
2024-03-01 12:13:40 +0100

    Small additions

--- src/main/java/de/nhausleitner/chess/adapter/web/WebController.java
+++ src/main/java/de/nhausleitner/chess/adapter/web/WebController.java
@@ -2,18 +2,35 @@ package de.nhausleitner.chess.adapter.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
 
     public WebController(){
-
     }
 
     @GetMapping("/")
     public String main(){
         return "main.html";
     }
+
+    @PostMapping("/neuesSpiel/freund")
+    public String neuesSpielGegenFreund(){
+        return "spielFreund.html";
+    }
+
+    @PostMapping("/neuesSpiel/karl")
+    public String neuesSpielGegenKarl(){
+        return "spielKarl.html";
+    }
+
+    @PostMapping("/neuesSpiel/stockfish")
+    public String neuesSpielGegenStockfish(){
+        return "spielStockfish.html";
+    }
+
+
 
 
 
